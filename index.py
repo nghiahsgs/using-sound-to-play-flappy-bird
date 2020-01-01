20352343e07139e6d773c64931c71e28d8e85cfa
nghiahsgs <nghiahsgs@gmail.com>
2020-01-01 13:20:38 +0700

    make it better

---
+++ index.py
@@ -0,0 +1,25 @@
+import os
+import time
+import speech_recognition as sr
+from gtts import gTTS
+
+def speak(text):
+	tts=gTTS(text=text,lang="en")
+	filename="voice.mp3"
+	tts.save(filename)
+	playsound.playsound(filename)
+
+def get_audio():
+	r=sr.Recognizer()
+	with sr.Microphone() as source:
+		audio=r.listen(source)
+		said=""
+		
+		try:
+			said=r.recognize_google(audio)
+			print(said)
+		except Exception as e:
+			print('Exception: '+str(e))
+			
+speak('hello')
+get_audio()
